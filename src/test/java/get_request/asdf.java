1337111b5afecefe387d15a2673e22f52948d2ac
tarikklckya <109520611+tarikklckya@users.noreply.github.com>
2022-11-13 18:09:12 +0300

    13.11.2022

---
+++ src/test/java/get_request/asdf.java
@@ -0,0 +1,25 @@
+package get_request;
+
+public class asdf {
+
+
+
+
+    /*
+    {
+    "status": "success",
+    "data": {
+        "employee_name": "Tiger Nixon",
+        "employee_salary": 320800,
+        "employee_age": 61,
+    },
+    "message": "Successfully! Record has been fetched."
+}
+     */
+
+
+
+
+
+
+}
